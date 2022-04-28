e598831851b4483f10057bc209a5f9ff161d8ed5
JChris246 <chris.401@live.com>
2022-04-28 11:42:03 -0400

    setup client

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="flex justify-center items-center bg-neutral-900 h-screen">
+            <h1 className="text-6xl text-gray-200">My Money Diary</h1>
+        </div>
+    );
+}
+
+export default App;
